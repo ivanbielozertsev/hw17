1559063a6e24dd340c7b7b02e63d39aafe6fc861
ivanbielozertsev <ivanbelozercev13@gmail.com>
2021-10-26 13:09:34 +0300

    Home Work 17

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
 const x = 4, y = 9;
 const R = 10;
-const lengthA = Math.sqrt(x * x + y * y);
+const lengthA = Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
 const checkA = lengthA < R ? alert('Точка А лежит в окружности') : alert('Точка А лежит за пределами окружности');
